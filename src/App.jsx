f8d970adb0ff23f16d6ec3130ba8405ee99156ad
Dhruv Vaviya <dhruvvaviyablog@gmail.com>
2026-04-28 12:03:02 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import Doctors from "./pages/Doctors"
+
+const App = () => {
+  return (
+    <div className="mx-4 sm:mx-[10%]">
+      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/doctors" element={<Doctors />} />
+        <Route path="/doctors:speciality" element={<Doctors />} />
+      </Routes>
+
+    </div>
+  )
+}
+
+export default App
